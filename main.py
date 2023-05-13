8474ad79d1a022ee7e5638419523d37140203855
Trần Ngọc Chinh <chinhtoan127@gmail.com>
2023-05-13 09:56:03 +0700

    đổi màu bảng

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from settings import *
-
+from level import Level
 
 class Game:
     def __init__(self):
@@ -8,7 +8,7 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption('Sprout land')  # đặt tên
         self.clock = pygame.time.Clock()
-
+        self.level = Level()
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -16,6 +16,7 @@ class Game:
                     pygame.quit()
                     sys.exit()
             dt = self.clock.tick() / 1000
+            self.level.run(dt)
             pygame.display.update()
 
 
