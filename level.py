cd8ed75fd9dee85b4318c0a00369b45182adecd8
Trần Ngọc Chinh <chinhtoan127@gmail.com>
2023-05-14 09:38:24 +0700

    taọ player đơn giản

--- level.py
+++ level.py
@@ -1,12 +1,14 @@
 import pygame
 from settings import *
-
+from player import Player
 
 class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()  # mô tả bề mặt hiển thị, có thể vẽ lên
         self.all_sprites = pygame.sprite.Group()
-
+        self.setup()
+    def setup(self):
+        self.player = Player((640,360),self.all_sprites)
     def run(self,dt):
         self.display_surface.fill('black')
         self.all_sprites.draw(self.display_surface)
