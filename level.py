9e9733e3c2b12a70b5a2d860fefb0cb229399544
Trần Ngọc Chinh <chinhtoan127@gmail.com>
2023-05-19 10:15:08 +0700

    tao vi tri bat dau cho player

--- level.py
+++ level.py
@@ -10,7 +10,7 @@ class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()  # mô tả bề mặt hiển thị, có thể vẽ lên
         self.all_sprites = CameraGroup()
-
+        self.collision_sprites = pygame.sprite.Group()
         self.setup()
         self.overlay = Overlay(self.player)
 
@@ -27,7 +27,7 @@ class Level:
                 Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites, LAYERS['main'])
         #fence hang rao
         for x, y, surf in tmx_data.get_layer_by_name('Fence').tiles():
-            Generic((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+            Generic((x * TILE_SIZE, y * TILE_SIZE), surf, [self.all_sprites, self.collision_sprites])
 
         # water
         water_frames = import_folder('D:/document/Năm 2/kỹ thuật lập trình python/s1 - setup/graphics/water')
@@ -35,13 +35,16 @@ class Level:
             Water((x * TILE_SIZE, y * TILE_SIZE),water_frames,self.all_sprites)
         #trees
         for obj in tmx_data.get_layer_by_name('Trees'):
-            Tree((obj.x,obj.y), obj.image,self.all_sprites,obj.name)
+            Tree((obj.x,obj.y), obj.image,[self.all_sprites, self.collision_sprites],obj.name)
 
         #wildflowers
         for obj in tmx_data.get_layer_by_name('Decoration'):
-            WildFlower((obj.x,obj.y), obj.image,self.all_sprites)
+            WildFlower((obj.x,obj.y), obj.image,[self.all_sprites, self.collision_sprites])
 
-        self.player = Player((640, 360), self.all_sprites)
+        #player
+        for obj in tmx_data.get_layer_by_name('Player'):
+            if obj.name =='Start':
+                self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
         Generic(
             pos=(0, 0),
             surf=pygame.image.load(
@@ -69,7 +72,7 @@ class CameraGroup(pygame.sprite.Group):
         self.offset.y = player.rect.centery - SCREEN_HEIGHT / 2
         for layer in LAYERS.values():
 
-            for sprite in self.sprites():
+            for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
                 if sprite.z == layer:
                     offset_rect = sprite.rect.copy()
                     offset_rect.center -= self.offset
--- player.py
+++ player.py
@@ -5,21 +5,27 @@ from timer import Timer
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, group):
+    def __init__(self, pos, group,collision_sprites):
         super().__init__(group)
 
         self.import_assets()
         self.status = 'down_idle'
         self.frame_index = 0
 
-        # setup
+        # general setup
         self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
         self.z =LAYERS['main']
 
+        #movement
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+
+        #collision_sprites
+        self.hitbox = self.rect.copy().inflate((-126, -70))
+        self.collision_sprites = collision_sprites
+
         # timer
         self.timers = {
             'tool use': Timer(350, self.use_tool),
@@ -114,16 +120,40 @@ class Player(pygame.sprite.Sprite):
         for timer in self.timers.values():
             timer.update()
 
+    def collision(self,direction):
+        for sprite in self.collision_sprites.sprites():
+            if hasattr(sprite,'hitbox'):
+                if sprite.hitbox.colliderect(self.hitbox):
+                    if direction == 'horizontal':
+                        if self.direction.x > 0: #move right
+                            self.hitbox.right = sprite.hitbox.left
+                        if self.direction.x < 0:  # move left
+                            self.hitbox.left = sprite.hitbox.right
+                        self.rect.centerx = self.hitbox.centerx
+                        self.pos.x = self.hitbox.centerx
+                    if direction == 'vertical':
+                        if self.direction.y > 0: #move down
+                            self.hitbox.bottom = sprite.hitbox.top
+                        if self.direction.y < 0: #move up
+                            self.hitbox.top = sprite.hitbox.bottom
+                        self.rect.centery = self.hitbox.centery
+                        self.pos.y = self.rect.centery
+
     def move(self, dt):
         # chuẩn hóa vector
         if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
         # chuyen dong ngang
         self.pos.x += self.direction.x * self.speed * dt
-        self.rect.centerx = self.pos.x
+        self.hitbox.centerx = round(self.pos.x)
+        self.rect.centerx = self.hitbox.centerx
+        self.collision('horizontal')
+
         # chuyen dong thang dung
         self.pos.y += self.direction.y * self.speed * dt
-        self.rect.centery = self.pos.y
+        self.hitbox.centery = round(self.pos.y)
+        self.rect.centery = self.hitbox.centery
+        self.collision('vertical')
 
     def update(self, dt):
         self.input()
