cc447630753707bd6d3aecd843341bd53e9191d0
Trần Ngọc Chinh <chinhtoan127@gmail.com>
2023-05-19 08:52:50 +0700

    tao cac hoạt ảnh như hoa và nấm

--- level.py
+++ level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from player import Player
 from overlay import Overlay
-from sprites import Generic, Water
+from sprites import Generic, Water,WildFlower
 from pytmx.util_pygame import load_pygame
 from support import *
 
@@ -34,7 +34,10 @@ class Level:
         for x, y, surf in tmx_data.get_layer_by_name('Water').tiles():
             Water((x * TILE_SIZE, y * TILE_SIZE),water_frames,self.all_sprites)
         #trees
+
         #wildflowers
+        for obj in tmx_data.get_layer_by_name('Decoration'):
+            WildFlower((obj.x,obj.y), obj.image,self.all_sprites)
 
         self.player = Player((640, 360), self.all_sprites)
         Generic(
